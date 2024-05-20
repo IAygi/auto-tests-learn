a311fddce1b8fec70089205be3fce1d314331855
Galyamskiy <xamach21@gmail.com>
2024-05-20 19:54:41 +0300

    Выполнил ДЗ

---
+++ src/test/java/ru/iaygi/api/tests/grigoriy/EndPoints.java
@@ -0,0 +1,7 @@
+package ru.iaygi.api.tests.grigoriy;
+
+public class EndPoints {
+    public static final String  BASE_URL = "https://reqres.in";
+    public static final String  USER_ID = "api/unknown/2";
+
+}
---
+++ src/test/java/ru/iaygi/api/tests/grigoriy/UserDto.java
@@ -0,0 +1,24 @@
+package ru.iaygi.api.tests.grigoriy;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+@Data
+@Accessors(fluent = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class UserDto {
+
+        private Integer id;
+        private String name;
+        private Integer year;
+        private String color;
+        @JsonProperty("pantone-value")
+        private String pantoneValue;
+    }
+
