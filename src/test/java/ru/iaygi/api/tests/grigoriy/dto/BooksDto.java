ac424680d5c26741a0c3389bfdd85ea6ce9639ad
Galyamskiy <xamach21@gmail.com>
2024-06-03 16:09:06 +0300

    Окончательные изменения по Дз по АПИ.

---
+++ src/test/java/ru/iaygi/api/tests/grigoriy/dto/BooksDto.java
@@ -0,0 +1,18 @@
+package ru.iaygi.api.tests.grigoriy.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+@Data
+@Accessors(fluent = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class BooksDto{
+	private List<BooksItemDto> books;
+}
