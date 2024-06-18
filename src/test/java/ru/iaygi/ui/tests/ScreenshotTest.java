9635fabb3751bf174a78959cb2cecefdbf3a6627
Innokentiy Aygi <iaygi@fil-it.ru>
2024-06-18 21:59:44 +0300

    Настройка Selenoid

--- src/test/java/ru/iaygi/ui/tests/ScreenshotTest.java
+++ src/test/java/ru/iaygi/ui/tests/ScreenshotTest.java
@@ -1,28 +1,27 @@
 package ru.iaygi.ui.tests;
 
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.junit5.TextReportExtension;
-import io.qameta.allure.*;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import ru.iaygi.ui.objects.ScreenshotMethods;
 import ru.iaygi.ui.objects.ScreenshotObject;
 import ru.iaygi.ui.service.TestBaseUi;
 import ru.iaygi.ui.service.TestResultLoggerExtension;
-
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static io.qameta.allure.SeverityLevel.CRITICAL;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.iaygi.common.EndPoints.*;
+import static ru.iaygi.common.EndPoints.ALLURE_REST_ASSURED;
+import static ru.iaygi.common.EndPoints.HABR_POST;
+import static ru.iaygi.ui.data.TestData.enableSelenoid;
 import static ru.iaygi.ui.objects.Elements.ARTICLE_SNIPPET;
 
 @Owner("iaygi")
 @Severity(CRITICAL)
-@Tag("ui_test")
 @Tag("regression")
 @Epic("WebSite")
 @Feature("Основная функциональность")
@@ -30,7 +29,7 @@ import static ru.iaygi.ui.objects.Elements.ARTICLE_SNIPPET;
 public class ScreenshotTest extends TestBaseUi {
 
     private ScreenshotObject screenshotObject = new ScreenshotObject();
-    private static final boolean USE_SELENOID = false;
+    private static final boolean USE_SELENOID = enableSelenoid;
 
     @BeforeAll
     public static void setUp() {
